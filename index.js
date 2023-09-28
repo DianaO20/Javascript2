0751d287f768dd258ea38cbbe3e8a31062153911
Diana <olednic79@gmail.com>
2023-09-28 20:32:54 +0300

    firts commit

---
+++ index.js
@@ -0,0 +1,108 @@
+const camelCase = 'camelCase';
+const UPPER_SNAKE_CASE ='SNAKE_CASE'
+const Capitale_Case = 'capitalCase'
+console.log(camelCase)
+console.log(UPPER_SNAKE_CASE)
+console.log(Capitale_Case)
+
+const str1 = 'Text'
+const str2 = 'something'
+const str3 = 'here'
+
+console.log(`${str1} ${str2} ${str3}`)
+
+const condition = true;
+if(condition){
+    console.log('Hello')
+
+}
+const falsy1 = 0;
+if(falsy1){
+    console.log('Hey')
+
+}else{
+    console.log('Bye')
+}
+const falsy2 =  '';
+if(falsy2){
+    console.log('Hey')
+
+}else{
+    console.log('Bye')
+}
+const condition3 = true;
+if(23>20){
+    console.log('Correct')
+
+}else{
+    console.log('It is not correct')
+}
+const falsy3 = undefined;
+if(falsy3){
+    console.log('Correct')
+
+}else{
+    console.log('It is not correct')
+}
+
+const falsy4 = null;
+if(falsy4){
+    console.log('Correct')
+
+}else{
+    console.log('It is not correct')
+}
+const falsy5 = NaN;
+if(falsy5){
+    console.log('Correct')
+
+}else{
+    console.log('It is not correct')
+}
+
+const falsy6 = true;
+if(falsy6){
+    console.log('Correct')
+
+}else{
+    console.log('It is not correct')
+}
+
+const summer = 0;
+const spring = 1
+if(summer || spring){
+    console.log('good weather')
+
+}else{
+    console.log('rainy days')
+}
+
+const summer1 = 0;
+const spring1 = 0;
+if(summer1 && spring1){
+    console.log('good weather')
+
+}else{
+    console.log('rainy days')
+}
+
+
+const variable = '12'
+console.log(typeof +variable)
+const variable1 = '12'
+console.log(typeof Number(variable1))
+const variable2 = '12'
+console.log(9 + + '12') 
+const variable3 = '12'
+console.log(Boolean(12)) 
+const variable6 = '12'
+console.log(Boolean('')) 
+
+const variable4 = 14
+console.log( typeof variable4.toString()) 
+const variable5 = 14
+console.log(Boolean(0)) 
+const variable7 = 14
+console.log(Boolean(567)) 
+ 
+
